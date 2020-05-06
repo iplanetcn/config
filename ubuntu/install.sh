bc682c50474b1f35593032dd912cbeda6ea25844
john <iplanetcn@gmail.com>
2020-05-06 23:28:55 +0800

    mod install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -6,3 +6,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 # install zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# install vundle.vim for vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
